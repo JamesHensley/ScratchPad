4794d52855a33ec94e2cc6a2c8c2069b2214d690
JamesHensley <JamesHensley@users.noreply.github.com>
2020-12-24 13:06:26 +0100

    Update PowershellHttpClient.ps1

--- PowershellHttpClient.ps1
+++ PowershellHttpClient.ps1
@@ -4,7 +4,6 @@ $Source = @"
     using System;
     using System.Net.Http;
     using System.Threading.Tasks;
-
     namespace IDST {
         public class Program
         {
@@ -16,34 +15,27 @@ $Source = @"
             
             public static async void testIt() {
                 Console.WriteLine("Inside testIt...");
-
                 Console.WriteLine("Downloading page...");
-
                 Task<string> task = DownloadPageAsync();
-                string something = await DownloadPageAsync();
+                string something = await task;
 
                 Console.WriteLine(something);
-
                 Console.WriteLine("All Finished...");
                 Console.ReadKey();
             }
-
             static async Task<string> DownloadPageAsync()
             {
                 Console.WriteLine("Inside DownloadPageAsync...");
-
                 string result = "";
                 await Task.Run(async () => {
                     Console.WriteLine("Inside Download");
                     string page = "http://en.wikipedia.org/";
-
                     using (HttpClient client = new HttpClient())
                     using (HttpResponseMessage response = await client.GetAsync(page))
                     using (HttpContent content = response.Content)
                     {
                         Console.WriteLine("Inside Usings....");
                         result = await content.ReadAsStringAsync();
-
                         if (result != null && result.Length >= 50)
                         {
                             Console.WriteLine(result.Substring(0, 50) + "...");
