55a66dcfe3dc0534dc398f95100ca43e3df5f288
JamesHensley <JamesHensley@users.noreply.github.com>
2020-12-25 18:48:49 +0100

    Update PowershellHttpClient.ps1

--- PowershellHttpClient.ps1
+++ PowershellHttpClient.ps1
@@ -1,22 +1,56 @@
-$Assem = ("System", "System.Net.Http", "System.Threading.Tasks")
+$Assem = ("System", "System.Net", "System.Net.Http", "System.Net.Http.WebRequest", "System.Threading.Tasks", "System.Security.Cryptography.X509Certificates")
 
 $Source = @"
 using System;
 using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
 using System.Net.Http;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
 
 namespace IDST
 {
     public class QlixHelper
     {
-        public static async Task<string> DownloadPageAsync(string downloadUrl)
+        public static X509Certificate2 GetCertWithThumb(string certThumbprint)
+        {
+            X509Certificate2 retCert = null;
+
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+            foreach (var x509 in store.Certificates)
+            {
+                Console.WriteLine(string.Format("Found Cert: {0}", x509.Thumbprint));
+                if (x509.Thumbprint.ToLower() == certThumbprint.ToLower())
+                {
+                    retCert = x509;
+                }
+            }
+            return retCert;
+        }
+
+        public static async Task<string> DownloadPageAsync(string downloadUrl) {
+            string result = "";
+            await Task.Run(async () =>
+            {
+                result = await DownloadPageAsync(downloadUrl, null);
+            });
+            return result;
+        }
+
+        public static async Task<string> DownloadPageAsync(string downloadUrl, X509Certificate2 clientCert)
         {
             string result = "";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
             await Task.Run(async () => {
-                using (HttpClient client = new HttpClient())
+                X509CertificateCollection certs = new X509CertificateCollection();
+                System.Net.Http.WebRequestHandler handler = new System.Net.Http.WebRequestHandler();
+                if (clientCert != null) { handler.ClientCertificates.Add(clientCert); }
+
+                using (HttpClient client = new HttpClient(handler))
                 using (HttpResponseMessage response = await client.GetAsync(downloadUrl))
                 using (HttpContent content = response.Content)
                 {
@@ -31,10 +65,12 @@ namespace IDST
 
 Add-Type -ReferencedAssemblies $Assem -TypeDefinition $Source -Language CSharp
 
-$task = [IDST.QlixHelper]::DownloadPageAsync("http://en.wikipedia.org/")
+$cert = [IDST.QlixHelper]::GetCertWithThumb("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+Write-Output $cert
+
+#$task = [IDST.QlixHelper]::DownloadPageAsync("http://en.wikipedia.org/")
+#$task = [IDST.QlixHelper]::DownloadPageAsync("https://webmail.XXXXX.XXX/my.policy#", $cert)
+
 while (-not $task.AsyncWaitHandle.WaitOne(200)) { }
-$something = $task.GetAwaiter().GetResult()
-
-
-Write-Output $something
-Read-Host -Prompt "Press Enter to continue"
+$taskReturn = $task.GetAwaiter().GetResult()
+Write-Output $taskReturn
